b93992ef0f32dbc94b87cd041988dcdd291436c2
sanjuv1 <sanjeev.khanth@icloud.com>
2020-08-16 23:50:27 +0530

    Sanjeeev it19248318

---
+++ app/src/main/java/com/example/juicehome/confirmation.java
@@ -0,0 +1,14 @@
+package com.example.juicehome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+    }
+}
