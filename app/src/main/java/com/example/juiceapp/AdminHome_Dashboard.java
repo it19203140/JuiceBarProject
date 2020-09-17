e8b98aa92dd4c721ba1eafcc27c859f2c74d7063
it19203140 <it19203140@my.sliit.lk>
2020-09-17 12:50:18 +0530

    Completed redirecting to appropriate pages

--- app/src/main/java/com/example/juiceapp/AdminHome_Dashboard.java
+++ app/src/main/java/com/example/juiceapp/AdminHome_Dashboard.java
@@ -1,14 +1,37 @@
 package com.example.juiceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AdminHome_Dashboard extends AppCompatActivity {
+
+    CardView inquiriesCard;
+    CardView ordersCard;
+    CardView productsCard;
+    CardView ingredientsCard;
+    CardView editProfileCard;
+    CardView aboutUsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home__dashboard);
+        productsCard = findViewById(R.id.cardViewProductsID);
+        redirectToProducts();
+
+    }
+
+    public void redirectToProducts() {
+        productsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(AdminHome_Dashboard.this, ViewProducts.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/juiceapp/ViewProducts.java
+++ app/src/main/java/com/example/juiceapp/ViewProducts.java
@@ -1,14 +1,30 @@
 package com.example.juiceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewProducts extends AppCompatActivity {
 
+    ImageView addProduct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_products);
+        addProduct = findViewById(R.id.imageViewAddProductID);
+        redirectToInsertProductActivity();
+    }
+
+    public void redirectToInsertProductActivity() {
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ViewProducts.this, InsertProduct.class));
+            }
+        });
     }
 }
